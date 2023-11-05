c129390b867c7188a1ca88ea49795bf427ec50b8
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-11-05 11:38:59 +0100

    zmiana config main.tf

--- main.tf
+++ main.tf
@@ -41,13 +41,3 @@ resource "azurerm_mssql_server" "sqlserver" {
   administrator_login_password = "papiezakGMD"
 }
 
-resource "azurerm_mssql_database" "sqldb" {
-  name           = "sqldbprjzesp"
-  server_id      = azurerm_mssql_server.sqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 5
-  read_scale     = false
-  sku_name       = "S0"
-  zone_redundant = false
-}
